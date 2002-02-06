9f396d5e2d0d5a0544e92dc7c1ed87926ca33f67
beaufour <>
2002-02-06 10:00:30 +0000

    Fixed some errors in the automake-system. Everything should work now.

---
+++ netdj/acconfig.h
@@ -0,0 +1,8 @@
+/* Define if you have icp_func defined */
+#undef HAVE_ICPFUNC
+
+/* Define if your readline library has `add_history' */
+#undef HAVE_READLINE_HISTORY
+
+/* Define if you want to enable readline support */
+#undef USE_READLINE
