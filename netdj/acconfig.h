4ead9b22fa948fdddab1b8815b21b00582747afe
beaufour <>
2002-02-16 14:42:22 +0000

    autoconf'ed the man-page.

--- netdj/acconfig.h
+++ netdj/acconfig.h
@@ -1,10 +1,13 @@
+/* Where to find local configuration files (~/CONF_DIRNAME) */
+#undef CONF_DIRNAME
+
 /* Define if you have icp_func defined */
 #undef HAVE_ICPFUNC
 
 /* Define if your readline library has `add_history' */
 #undef HAVE_READLINE_HISTORY
 
-/* Where to find configuration files */
+/* Where to find global configuration files */
 #undef NETDJ_ETCDIR
 
 /* Define if you want to enable readline support */
