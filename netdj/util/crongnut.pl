36d344e417584a7f638bfb249edc7fe2edbc80cf
beaufour <>
2002-02-22 13:47:03 +0000

    Fixed startup-bug.

--- netdj/util/crongnut.pl
+++ netdj/util/crongnut.pl
@@ -4,23 +4,30 @@ my $THRES = 20 * 60; # 20 minutes
 my $DIR = "/home/beaufour/netdj/cache";
 my $WD = "                          ";
 
-print "Gnut watchdog v0.1\n";
+print "Gnut watchdog v0.2\n";
 print "------------------\n";
 
 while (1) {
   # Count the cattle
   my $latest = check_gnut("$DIR/*");
 
-  print "[".localtime()."] Latest filechange was $latest seconds ago\n";
+  print "[".localtime()."] Latest filechange was ".($latest/60)." minutes ago\n";
 
-  my $gnutproc = `ps --no-headers -C gnut`;
+  my $gnutproc = check_proc();
 
   if (!$gnutproc || $latest >= $THRES) {
-    print "$WD   Hmmm, somebody is sleep on the job!\n";
     if ($gnutproc) {
-      print "$WD   Killing gnut\n";
-      `killall -9 gnut`;
-      sleep 10;
+      print "$WD   Hmmm, somebody is sleeping on the job!\n";
+      print "$WD   Telling it to quit nicely\n";
+      `killall gnut`;
+      sleep 5;
+      if (check_proc()) {
+	print "$WD   Apparently not listening to nice talk, giving it the axe!\n";
+	`killall -9 gnut`;
+	sleep 5;
+      }
+    } else {
+      print "$WD   Seems like gnut's not running\n"; 
     }
 
     print "$WD   Removing any leftover .gnut's\n";
@@ -31,11 +38,13 @@ while (1) {
 
     print "$WD   Spawn new gnut-daemon\n";
     `gnut -d > gnut.log 2> gnut.errlog &`;
+      
+    $latest = 0;  
   }
 
   # Sleeping my day away
   print "[".localtime()."] Waiting ".(($THRES - $latest)/60)." minutes ...\n";
-  sleep $THRES - $latest;
+  sleep ($THRES - $latest);
 }
 
 sub check_gnut {
@@ -48,4 +57,8 @@ sub check_gnut {
     }
   }
   return $newest;
+}
+
+sub check_proc {
+  return `ps --no-headers -C gnut`;
 }
